d1d6b6d897a5c828b388be7d625d24e54f5aac92
gabed <78431504+gdolsten@users.noreply.github.com>
2024-09-25 11:26:06 -0400

    Updating sequence objects

--- seq_to_pheno/longevity/run_esm.sh
+++ seq_to_pheno/longevity/run_esm.sh
@@ -5,3 +5,11 @@ protein_name=$1
 
 # Use the variable in the file paths
 esm-extract esm2_t33_650M_UR50D examples/all_${protein_name}.fa examples/test_${protein_name}_emb --repr_layers 0 32 33 --include mean
+
+esm-extract esm2_t33_650M_UR50D input/all_protein_sequences.fasta output/all_protein_sequences_emb --repr_layers 0 32 33 --include mean
+
+
+
+
+
+
